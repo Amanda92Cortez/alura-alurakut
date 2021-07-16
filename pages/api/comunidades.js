dc34896b6cb43571662b5863cbc80b016b48b56c
Amanda92Cortez <amanda_cortez_2012@hotmail.com>
2021-07-16 12:29:50 -0300

    comentários das alterações

---
+++ pages/api/comunidades.js
@@ -0,0 +1,29 @@
+import { SiteClient } from 'datocms-client';
+
+export default async function recebedorDeRequests(request, response) {
+    if(request.method === 'POST') {
+        const TOKEN = 'b971ca8d7ea219aa2bcddc28fe543c';
+        const client = new SiteClient(TOKEN);
+        
+        // Validar os dados, antes de sair cadastrando
+        const registroCriado = await client.items.create({
+            itemType: "972293", // ID do Model de "Communities" criado pelo Dato
+            ...request.body,
+            // title: "Comunidade de Teste",
+            // imageUrl: "https://github.com/amanda92cortez.png",
+            // creatorSlug: "amanda92cortez"
+        })
+    
+        console.log(registroCriado);
+    
+        response.json({
+            dados: 'Algum dado qualquer',
+            registroCriado: registroCriado,
+        })
+        return;
+    }
+
+    response.status(404).json({
+        message: 'Ainda não temos nada no GET, mas no POST tem!'
+    })
+}
